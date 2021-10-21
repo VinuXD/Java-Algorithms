263f220b9d572e375858f17f72b3426ddec3536a
VinuXD <vinuvarsath3@gmail.com>
2021-10-21 11:51:16 +0530

    Test Commit

---
+++ Maths/BasicOperators.java
@@ -0,0 +1,20 @@
+// Author github.com/VinuXD
+
+class BasicOperators {
+    public static void main(String args[]) {
+        int a = Integer.parseInt(args[0]); // Getting a in Command Line as a Integer
+        int b = Integer.parseInt(args[1]); // Getting b in Command Line as a Integer
+        int d,s;
+        s = a + b; // Storing value of a+b in varibale s
+        if(a>b) // Yes, We don't need answer in negative values!
+         d = a - b;
+        else
+         d = b - a;
+        System.out.println("Sum        : " +s);
+        System.out.println("Difference : " +d);
+        System.out.println("Product    : " +a*b);
+        System.out.println("Quotient   : " +a/b); // Dividing by Zero can cause Exception
+        System.out.println("Reminder   : " +a%b);
+        
+    }
+}
